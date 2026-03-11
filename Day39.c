dc3bb3f49edba4a688b3c9858f114c7e09151013
Pallavi Yadav <pallaviyadav.py7897@gmail.com>
2026-03-11 17:06:39 +0530

    Add files via upload

---
+++ Day39.c
@@ -0,0 +1,101 @@
+//Implement a Min Heap using an array where the smallest element is always at the root.
+
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+int heap[MAX];
+int size = 0;
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void heapifyUp(int i)
+{
+    while (i > 0 && heap[(i-1)/2] > heap[i])
+    {
+        swap(&heap[i], &heap[(i-1)/2]);
+        i = (i-1)/2;
+    }
+}
+void heapifyDown(int i)
+{
+    int smallest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+    if (left < size && heap[left] < heap[smallest])
+        smallest = left;
+    if (right < size && heap[right] < heap[smallest])
+        smallest = right;
+    if (smallest != i)
+    {
+        swap(&heap[i], &heap[smallest]);
+        heapifyDown(smallest);
+    }
+}
+void insert(int x)
+{
+    heap[size] = x;
+    size++;
+    heapifyUp(size-1);
+}
+int extractMin()
+{
+    if (size == 0)
+        return -1;
+    int root = heap[0];
+    heap[0] = heap[size-1];
+    size--;
+    heapifyDown(0);
+    return root;
+}
+int peek()
+{
+    if (size == 0)
+        return -1;
+    return heap[0];
+}
+int main()
+{
+    int n, value;
+    char op[20];
+    printf("Enter number of operations: ");
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        printf("Enter operation: ");
+        scanf("%s", op);
+        if(strcmp(op, "insert") == 0)
+        {
+            printf("Enter value: ");
+            scanf("%d", &value);
+            insert(value);
+        }
+        else if(strcmp(op, "extractMin") == 0)
+        {
+            printf("%d\n", extractMin());
+        }
+        else if(strcmp(op, "peek") == 0)
+        {
+            printf("%d\n", peek());
+        }
+    }
+    return 0;
+}
+
+/*Output
+Enter number of operations: 6
+Enter operation: insert
+Enter value: 40
+Enter operation: insert
+Enter value: 10
+Enter operation: insert
+Enter value: 20
+Enter operation: peek
+10
+Enter operation: extractMin
+10
+Enter operation: peek
+20
+*/
